85d159a81a654ca1d6e7632b7e69bfa944b433dd
Tahmid Hasan <tmd999510@gmail.com>
2025-08-29 12:34:00 +0600

    Call buttom functionality updated

--- emergencyService.js
+++ emergencyService.js
@@ -4,55 +4,147 @@ let h_count= parseInt( document.getElementById("heart_count").innerText)
 document.getElementById("heart_icon_1").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 1 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_2").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 2 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_3").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 3 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_4").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 4 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_5").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 5 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_6").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 6 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_7").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 7 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_8").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 8 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 document.getElementById("heart_icon_9").addEventListener("click",function(e){
     e.preventDefault();
     h_count++;
-    console.log("Heart icon 9 click");
     document.getElementById("heart_count").innerText =h_count;
 })
 // -------------------------**************------------------------------
+let c_count= parseInt(document.getElementById("coin_count").innerText)
+document.getElementById("call_btn_1").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling National Emergency Service 999.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_2").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Police Helpline Service 999.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_3").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Fire Service 999.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_4").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Ambulance Service 1994-99999.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_5").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Women & Child Helpline Service 109.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+//Anti-Corruption Helpline
+document.getElementById("call_btn_6").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Anti-Corruption Helpline Service 106.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_7").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Electricity Helpline Service 16216.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_8").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Brac Helpline Service 16445.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
+document.getElementById("call_btn_9").addEventListener("click",function(e){
+    e.preventDefault();
+    if(c_count >= 20){
+        c_count -=20;
+        alert("Calling Bangladesh Railway Helpline Service 163.....")
+        document.getElementById("coin_count").innerText = c_count;
+    }
+    else{
+        return alert("You don't have enough coin to Call.")
+    }
+})
