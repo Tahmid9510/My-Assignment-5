e22ae630f6a8b33884eabecdb4bdae7c952411b9
Tahmid Hasan <tmd999510@gmail.com>
2025-08-29 11:21:38 +0600

    Heart Icon functionalites updated

---
+++ emergencyService.js
@@ -0,0 +1,58 @@
+// -------------------------For heart Icon------------------------------
+let h_count= parseInt( document.getElementById("heart_count").innerText)
+
+document.getElementById("heart_icon_1").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 1 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_2").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 2 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_3").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 3 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_4").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 4 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_5").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 5 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_6").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 6 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_7").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 7 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_8").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 8 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+document.getElementById("heart_icon_9").addEventListener("click",function(e){
+    e.preventDefault();
+    h_count++;
+    console.log("Heart icon 9 click");
+    document.getElementById("heart_count").innerText =h_count;
+})
+// -------------------------**************------------------------------
